2c16719ad78821e1f5ee8f0e6a404fb04fbe36e5
VuHuy <huyv80313@gmail.com>
2024-06-27 16:22:48 +0700

    Site updated: 2024-06-27 16:22:47

--- js/Capoo.js
+++ js/Capoo.js
@@ -1,19 +1,16 @@
-// 创建一个新的图片元素并添加到页面中
 function createGifElement() {
     var gifContainer = document.createElement('div');
     gifContainer.classList.add('gif-container');
 
     var gifImage = document.createElement('img');
-    gifImage.src = '../img/Capoo/b.gif'; // 修改为你的GIF图像路径
-    gifImage.alt = 'Capoo GIF Image'; // 修改为你的GIF图像的alt文本
-
+    gifImage.src = '../img/Capoo/nice.gif'; 
+    gifImage.alt = 'Capoo GIF Image'; 
     gifContainer.appendChild(gifImage);
     document.body.appendChild(gifContainer);
 
     return gifContainer;
 }
 
-// 检测设备类型并在移动设备上隐藏 GIF 图像
 function checkDeviceType() {
     var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
     var gifContainer = createGifElement();
@@ -22,7 +19,6 @@ function checkDeviceType() {
     }
 }
 
-// 当页面加载完成时检测设备类型
 document.addEventListener('DOMContentLoaded', function () {
     checkDeviceType();
 });
